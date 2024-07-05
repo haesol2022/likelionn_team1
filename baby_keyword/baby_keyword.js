d18897d4148c5d3b31da2fe40acf2305629a8da6
haeuuni <373seoha@gmail.com>
2024-07-05 20:29:57 +0900

    :1

--- baby_keyword/baby_keyword.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('complete-btn').addEventListener('click', function() {
-        window.location.href = '../me_keyword/me_keyword.html';
-    });
-});
